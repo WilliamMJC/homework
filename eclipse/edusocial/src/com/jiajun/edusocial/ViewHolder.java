c7c859ba9a5febe00e4d76112ffd0112334c5a8f
msleo78 <543847182@qq.com>
2015-05-01 13:18:12 +0800

    a

--- eclipse/edusocial/src/com/jiajun/edusocial/ViewHolder.java
+++
@@ -1,9 +0,0 @@
-package com.jiajun.edusocial;
-
-import android.widget.CheckBox;
-import android.widget.TextView;
-
-public class ViewHolder {
-	CheckBox cbclass = null;
-	TextView tvclass = null;
-}
