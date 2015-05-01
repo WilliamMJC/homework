8d404f87e3ef810bedc96785188bd68d973ba32b
msleo78 <543847182@qq.com>
2015-05-01 14:42:54 +0800

    123

--- eclipse/edusocial/src/com/jiajun/edusocial/SelectClaSch/ViewHolder.java
+++
@@ -1,9 +0,0 @@
-package com.jiajun.edusocial.SelectClaSch;
-
-import android.widget.CheckBox;
-import android.widget.TextView;
-
-public class ViewHolder {
-	CheckBox cbclass = null;
-	TextView tvclass = null;
-}
